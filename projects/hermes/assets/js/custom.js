bec679c111c32d20d7bc48e7383406935520bd5b
Diana Wang <dwang98115@gmail.com>
2017-12-07 22:32:40 -0800

    updated header, features section, and video, tweaked javascript

--- projects/hermes/assets/js/custom.js
+++ projects/hermes/assets/js/custom.js
@@ -10,4 +10,38 @@ window.onscroll = () => {
   	}
 };
 
+const video_link = $('#video_link');
+const popup = $("#dialog_popup");
+const video = $("#dialog_video");
 
+
+video_link.click(function() {
+	console.log("clicked outside");
+});
+
+popup.dialog({
+    zIndex: 999,
+    autoOpen: false,
+    modal: true,
+    width:'auto',
+    "X": function () {
+        $(this).dialog("close");
+    },
+    open: function (event, ui) {
+        $('.ui-widget-overlay').addClass('override');
+        $("body").css("overflow", "hidden");
+        $(".ui-button").removeClass("ui-state-hover");
+        video[0].load();
+    },
+    close: function (event, ui) {
+    	$("body").css("overflow", "auto");
+    	video[0].pause();
+    }
+});
+
+
+video_link.click(function() {
+	console.log("clicked inside");
+    popup.dialog("open");
+    return false;
+});
